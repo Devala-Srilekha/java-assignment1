4c22142978d83e421da03daeff52166005bb3669
Devala-Srilekha <57786670+Devala-Srilekha@users.noreply.github.com>
2019-11-15 12:04:38 +0530

    Add files via upload

---
+++ Palindromecheck.java
@@ -0,0 +1,16 @@
+package javaproject.sample;
+
+
+public class Palindromecheck {
+	   public static boolean isPalindrome(String String)
+	   {
+		   if(String.length()==0 || String.length()==1)
+			   return true;
+		   if(String.charAt(0)==String.charAt(String.length()-1))
+			   return isPalindrome(String.substring(1,String.length()-1));
+		   return false;
+	   
+	    }
+	}
+
+
