e58a615d41a459688a56e05d2a192ed3220dd2d9
Devala-Srilekha <57786670+Devala-Srilekha@users.noreply.github.com>
2019-12-09 12:21:20 +0530

    Add files via upload

---
+++ practice questions/Assignment 3/Assignment3 test cases/BinarysearchTest.java
@@ -0,0 +1,66 @@
+package omniwyse.Assignment3;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class BinarysearchTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//1)---CREATING A BINARY SEARCH TREE-----
+		
+				Binarysearch tree = new Binarysearch();
+				 	
+				 //2)-----INSERTING ELEMENTS IN TO TREE-----
+				 
+				 	tree.insert(50); 
+			        tree.insert(30); 
+			        tree.insert(20); 
+			        tree.insert(40); 
+			        tree.insert(70); 
+			        tree.insert(60); 
+			        tree.insert(80); 
+			  
+			      //3)----TRAVERSING TREE IN INORDER(),PREORDER(),POSTORDER()
+			        System.out.print("InOrder Traversal:");
+			        tree.printInorder(tree.root);
+			        System.out.println();
+			        System.out.print("PreOrder Traversal:");
+			        tree.printPreorder(tree.root);
+			        System.out.println();
+			        System.out.print("PostOrder Traversal:");
+			        tree.printPostorder(tree.root);
+			        System.out.println();
+			        
+			       //4)--SEARCHING AN ELEMENT IN BST-----
+			        
+			        System.out.println(tree.search(tree.root, 20)+" is found");
+			        
+			     //5)---DELETE ELEMENT FROM BST----
+			        tree.deleteKey(20);
+			        
+			      // ---TREE AFTER DELETING ELEMENT
+			        System.out.println("After deleting element tree is:");
+			        tree.printInorder(tree.root);
+			        
+			      //6)---HEIGHT OR DEPTH OF TREE---
+			        System.out.println();
+			        System.out.println("Height of tree is:"+tree.maxDepth(tree.root));
+			        
+			      //7)---NUMBER OF LEAVES---
+			        System.out.println("Number of leaves are:"+tree.getLeafCount(tree.root));
+			        
+			        
+			}
+			
+
+		
+	}
+
+
---
+++ practice questions/Assignment 3/Assignment3 test cases/BonusTest.java
@@ -0,0 +1,45 @@
+package omniwyse.Assignment3;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import omniwyse.Assignment3.Bonus.KStack;
+
+public class BonusTest {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		 // Let us create 3 stacks in an array of size 10 
+        int k = 3, n = 10; 
+          
+        KStack ks = new KStack(k, n); 
+  
+        ks.push(15, 2); 
+        ks.push(45, 2); 
+  
+        // Let us put some items in stack number 1 
+        ks.push(17, 1); 
+        ks.push(49, 1); 
+        ks.push(39, 1); 
+  
+        // Let us put some items in stack number 0 
+        ks.push(11, 0); 
+        ks.push(9, 0); 
+        ks.push(7, 0); 
+  
+        System.out.println("Popped element from stack 2 is " + ks.pop(2)); 
+        System.out.println("Popped element from stack 1 is " + ks.pop(1)); 
+        System.out.println("Popped element from stack 0 is " + ks.pop(0)); 
+	}
+
+
+
+	}
+
+
